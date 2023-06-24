8b8a118428a26adc4a27aad9c0486f2950625d78
Caue15 <69868924+Caue15@users.noreply.github.com>
2023-06-24 20:11:32 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 function Home() {
-    return <h1>Sophia, eu amo você. Se você me ama, dá uma risadinha! (P.S.: Amo o Bentolitos também!)</h1>
+  return (
+    <h1>
+      Sophia, eu amo você. Se você me ama, dá uma risadinha! (P.S.: Amo o
+      Bentolitos também!)
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identação errada");
 }
 
 export default Home;
