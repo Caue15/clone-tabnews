c67dac8917e3a20f9bb17c12afb01be11d393136
Caue15 <69868924+Caue15@users.noreply.github.com>
2025-04-21 21:29:18 +0000

    add `database.js` and `.env` files

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <h1>
-      Sosô, eu te amo!! Se você também me ama, dá uma risadinha! &#128526;
+      Nati, eu te amo!! Se você também me ama, dá uma risadinha! &#128526;
       &#128525; <span style={{ color: "red" }}>&#10084;</span>
     </h1>
   );
