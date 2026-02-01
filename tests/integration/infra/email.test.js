04d20d4f08d269d1af9a6f184299c056aac32f14
Caue15 <69868924+Caue15@users.noreply.github.com>
2026-02-01 20:32:35 +0000

    feat: add `features` column to `users` table

--- tests/integration/infra/email.test.js
+++ tests/integration/infra/email.test.js
@@ -11,21 +11,21 @@ describe("infra/email.js", () => {
 
     await email.send({
       from: "Cauê <caue.renatini@gmail.com>",
-      to: "contato@cauertg.com",
+      to: "contato@gmail.com",
       subject: "Teste de assunto",
       text: "Teste de corpo.",
     });
 
     await email.send({
       from: "Cauê <caue.renatini@gmail.com>",
-      to: "contato@cauertg.com",
+      to: "contato@gmail.com",
       subject: "Último email enviado",
       text: "Corpo do último email.",
     });
 
     const lastEmail = await orchestrator.getLastEmail();
     expect(lastEmail.sender).toBe("<caue.renatini@gmail.com>");
-    expect(lastEmail.recipients[0]).toBe("<contato@cauertg.com>");
+    expect(lastEmail.recipients[0]).toBe("<contato@gmail.com>");
     expect(lastEmail.subject).toBe("Último email enviado");
     expect(lastEmail.text).toBe("Corpo do último email.\r\n");
   });
